29edd3c6ace0a3d5830ef3b8cb250497f91abf04
Deepanshu Yadav <deepanshu.yadav@qsstechnologies.com>
2022-06-28 13:55:10 +0530

    added packageButton

--- Screens/Home/Home.js
+++ Screens/Home/Home.js
@@ -1,11 +1,22 @@
 import React from "react";
-import { View, Text} from "react-native"
+import { View, Text, Dimensions} from "react-native"
+import PackageButton  from '../../Components/Home/packageButton';
+const { height, width } = Dimensions.get('window')
+
+const con = () =>{
+    console.log("hii");
+}
 
 export default function Home(){
 
     return (
         <View>
-            <Text>Home Screen</Text>
+            <View style={{ paddingHorizontal: 20, marginTop: 20, flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between' }}>
+                <PackageButton oncall={con} width={width} iconName="calendar" text1="Book an Appointment" text2="with a GP or Nune" ></PackageButton>
+                <PackageButton oncall={con} width={width} iconName="calendar" text1="Book an Appointment" text2="with a GP or Nune" ></PackageButton>
+                <PackageButton oncall={con} width={width} iconName="calendar" text1="Book an Appointment" text2="with a GP or Nune" ></PackageButton>
+                <PackageButton oncall={con} width={width} iconName="calendar" text1="Book an Appointment" text2="with a GP or Nune" ></PackageButton>
+            </View>
         </View>
     );
 
